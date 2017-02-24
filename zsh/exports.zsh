ca94882c45b78b0dea565ccfb8d52f399e74785d
Jack Blandin <jack@trunkclub.com>
2017-02-24 11:01:15 -0600

    Adds NPM + NVM setup so that TC Catalog App runs okay

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,10 +2,18 @@ export DOCKER_HOST=tcp://192.168.23.2:2375
 export PATH="$HOME/.rbenv/bin:$PATH" # Add ~/.rbenv/bin to your $PATH for access to the rbenv command-line utility.
 export EDITOR=/usr/local/bin/vim
 export VISUAL=/usr/local/bin/vim
-export NVM_DIR=~/.nvm
 export LOG_FORMAT="colored"
 export ZSH=$HOME/.oh-my-zsh
 export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# NPM
+export NPM_TOKEN=""
+export PATH=$PATH:~/npm
+
+# NVM
+export NVM_DIR=~/.nvm
+export PATH=$PATH:~/nvm
+[[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"  # This loads NVM
 
 # Go PATH
 export GOPATH=$HOME/Go
