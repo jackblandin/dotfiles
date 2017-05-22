29c744da21e041d8b4ce5182e6768c3243862a8a
Jack Blandin <jack@trunkclub.com>
2017-05-22 16:37:09 -0500

    Remove npm token

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,7 +7,6 @@ export ZSH=$HOME/.oh-my-zsh
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # NPM
-export NPM_TOKEN="02c53ebe-2115-44b9-a2be-cb7bba3d4950" # TC registry
 export PATH=$PATH:~/npm
 
 # NVM
@@ -26,7 +25,6 @@ export PATH="/Users/JackBlandin/miniconda2/bin:$PATH"
 
 # Data Science database config
 export DB_CONFIG=~/Desktop/data_science_demystified/config.json                                                                      
-export PRODUCT_SVC_DATABASE_URL="postgres://product_svc_user:bfUm4UFWBhdMaXFTYt6JmEr@sql-mrc01-prd.crsgng5grivx.us-east-1.rds.amazonaws.com/product_svc?pool=24"
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/Cellar/openssl/1.0.2j/lib
 export DYLD_FALLBACK_LIBRARY_PATH=$HOME/miniconda2/lib/:$DYLD_FALLBACK_LIBRARY_PATH
 
