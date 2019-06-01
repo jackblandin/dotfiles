c151af52875f2356bc57ac879cb14e56ed320385
Jack Blandin <jackblandin@gmail.com>
2019-06-01 15:58:55 -0500

    Fix minconda source path, add jdb_solarized_dark.vim

--- zsh/evals.zsh
+++ zsh/evals.zsh
@@ -1,5 +1,5 @@
 eval "$(rbenv init -)" # Add rbenv init to your shell to enable shims and autocompletion
 # eval $(docker-machine env) # set docker environment variables
 
-# OCAML
-eval `opam config env`
+# # OCAML
+# eval `opam config env`
